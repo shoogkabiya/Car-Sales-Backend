c17d36a6f1fb3afd932f7e199e1d68f5ce7205e3
shoogkabiya <shoogkabiya@hotmail.com>
2021-09-15 02:00:27 +0300

    add handlers and start to working on signup to the consumer

---
+++ handlers/consumers/signup.js
@@ -0,0 +1,34 @@
+// models imports
+const Consumer = require("../../database/models/consumer");
+
+const signup = async (req, res) => {
+  const {
+    firstname,
+    lastname,
+    email,
+    consumer_password,
+    confirm_consumer_password,
+  } = req.body;
+
+  try {
+    const consumerfound = await Consumer.findOne({ email });
+
+    if (consumerfound) {
+      return res.status(400).json("email already exists");
+    }
+
+    const newConsumer = new Consumer({
+      firstname,
+      lastname,
+      email,
+      consumer_password,
+      confirm_consumer_password,
+    });
+
+    newConsumer.save().then((newconsumer) => {
+      return res.status(200).json(newconsumer);
+    });
+  } catch (error) {
+    res.status(500).json("server error");
+  }
+};
