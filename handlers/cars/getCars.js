3915bb3716a3fd45556292d35cbe8068dd51f698
shoogkabiya <shoogkabiya@hotmail.com>
2021-11-17 21:18:11 +0200

    finish working on two routers addCars and getCars

--- handlers/cars/getCars.js
+++ handlers/cars/getCars.js
@@ -2,7 +2,6 @@
 const Car = require("../../database/models/cars");
 
 const getCars = async (req, res, next) => {
-  const id = req.body;
   let carsObject = await Car.find();
   return res.status(200).json(carsObject);
 };
